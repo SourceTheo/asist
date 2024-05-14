3e71cf4a4bdd1de45da2b948537cb9122d732e73
SourceTheo <xixtheox@gmail.com>
2024-05-14 16:35:07 +0300

    Add files via upload

---
+++ resources/Formation/animation/_an.py
@@ -0,0 +1,13 @@
+#   Theo - USERBOT
+#   TELE - @Source_Theo 
+
+#  ======================================================= #
+
+from userbot.Config import Config
+
+USERID = Config.OWNER_ID 
+ALIVE_NAME = Config.ALIVE_NAME
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Zed Userbot"
+mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
+
+#  ======================================================= #
